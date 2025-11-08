236b92c408454018668bb3e88717bef481207c2e
samoswald270800-spec <samoswald270800@gmail.com>
2025-11-07 21:02:13 -0500

    Update server.js

--- server.js
+++ server.js
@@ -488,7 +488,7 @@ ${assignment}
           threshold: 0.5,
           prefix_padding_ms: 300,
           silence_duration_ms: 1200,
-          create_response: false,
+          create_response: true,
           interrupt_response: true
         },
        instructions: `
